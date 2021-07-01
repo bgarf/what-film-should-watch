7074e63911d1d07b76447e5be82e34d7a05641a8
bethanie-garfin <bethanie.garfin@autotrader.co.uk>
2021-07-01 21:05:21 +0100

    Fix styling and add placeholder film results

--- src/index.js
+++ src/index.js
@@ -1,27 +1,59 @@
 
 import ReactDOM from 'react-dom';
 import React from 'react';
-import './css/index.css'
+import styles from './css/index.css'
 
+
+class SearchBar extends React.Component {
+  render () {
+      return (
+        <form>
+          <input className={styles.searchInput} type="text" name="search-params" placeholder="What type of film..."/>
+          <input type="submit"/>
+        </form>
+      )
+
+  }
+}
+
+class Navigation extends React.Component {
+   render () {
+     return (
+        <div className={styles.navbar}>
+        <a href="#" onClick={() => this.props.handleClick()}>All Films</a>
+        <a className={styles.centre} href="#">Pick A Genre</a>
+        <a href="#">Ratings</a>
+        </div>
+     )
+   }
+}
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      films: []
+    };
+  }
+
+  handleClick() {
+    this.setState({films:["film 1", "film 2", "film 3"]})
+  }
+
   render () {
     return (
       <div>
         <h1>What film should I watch?</h1>
-        <form className="search">
-        <div className="search-outer-layer">
-          <input id="search-input" type="text" name="search-params"
-          placeholder="What type of film..."/>
-        </div>
-        <input type="submit"/>
-        </form>
 
+        <SearchBar />
 
-        <div className="navbar">
-        <a className="options" href="#">All Films</a>
-        <a className="options centre" href="#">Pick A Genre</a>
-        <a className="options" href="#">Ratings</a>
+        <Navigation handleClick={() => this.handleClick()}/>
+
+        <div id="results"> 
+          { this.state.films.map((v, i) => {
+            return <h1>{v},{i}</h1>
+          })}
         </div>
+
       </div>
     );
   }
